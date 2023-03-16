f7e1f190a5b7f9de00f0a466f31ba3f1b27cb1fd
kangieG <kangiegladys@gmail.com>
2023-03-16 14:51:08 -0700

    Size of types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 /**
-*main - The program prints the size of various types on the computer it is compiled and run on
+*main-Program prints size of the types on computer it is compiled and run on
 *Return:0
 */
-int main (void);
+int main(void);
 {
 char a;
 int b;
@@ -15,6 +15,6 @@ printf("Size of a char: %lu bytes(s)\n", (unsigned long)sizeof(a));
 printf("Size of an int: %lu bytes(s)\n", (unsigned long)sizeof(b));
 printf("Size of a long int: %lu bytes(s)\n", (unsigned long)sizeof(c));
 printf("Size of a long long int: %lu bytes(s)\n", (unsigned long)sizeof(d));
-printf("Size of a float: %lu bytes(s)\n", (unsigned)sizeof(f));
+printf("Size of a float: %lu bytes(s)\n", (unsigned int)sizeof(f));
 return (0);
 }
