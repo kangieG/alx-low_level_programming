9e40dcd66161dfd870944c8df17faba939b9668e
kangieG <kangiegladys@gmail.com>
2023-03-20 09:33:38 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main- Entry point
+ *Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char gladys;
+
+	for (gladys = 'a'; gladys <= 'z'; gladys++)
+		putchar(gladys);
+	putchar('\n');
+
+	return (0);
+}
